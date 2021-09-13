6ffec3ea8a186ecdb950462d6b8030f0ca6757bd
Luis Corrales Marin <luiscorrales5012@gmail.com>
2021-09-13 07:59:16 -0500

    caja de ritmos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from './components/App';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// caja
+/* global React, ReactDOM */
+/* eslint-disable react/prop-types, react/no-multi-comp */
+
+// eslint-disable-next-line no-unused-vars
+const projectName = 'drum-machine';
+
+// coded by @no-stack-dub-sack (github) / @no_stack_sub_sack (codepen)
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
